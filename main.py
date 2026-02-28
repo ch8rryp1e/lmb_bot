720f8b4afc5b3dd4b98ec5beda8e2ebbebaa947c
cherrypie <zBeAlqc@gmail.com>
2026-03-01 02:04:49 +0500

    Refactor bot and dispatcher initialization

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import logging
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+from config_reader import config
 from handlers.basic_handlers import router
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token="")
-dp = Dispatcher()
 
 async def main():
-    
+    bot = Bot(token=config.bot_token.get_secret_value())
+    dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
